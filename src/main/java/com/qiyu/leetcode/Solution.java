b62a7aae4e2a017d3e7169acf1b5cced505cbdda
niyalishanda <35219349+niyalishanda@users.noreply.github.com>
2022-03-18 21:59:31 +0800

    Update Solution.java
    实现239

--- src/main/java/com/qiyu/leetcode/Solution.java
+++ src/main/java/com/qiyu/leetcode/Solution.java
@@ -1,5 +1,34 @@
 package com.qiyu.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Solution {
 
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new LinkedList<>();
+
+        for(int i = 0; i < k; i++) {
+            while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+
+        int[] ans = new int[nums.length - k + 1];
+        ans[0] = nums[dq.peekFirst()];
+        for(int i = k; i < nums.length; i++) {
+            while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+
+            if(dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            ans[i - k + 1] = nums[dq.peekFirst()];
+        }
+        return ans;
+    }
 }
