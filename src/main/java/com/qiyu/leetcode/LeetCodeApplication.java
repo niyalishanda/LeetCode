11bc996c3d1348b910138c1929f69e6b8f3d9c72
niyalishanda <35219349+niyalishanda@users.noreply.github.com>
2022-05-24 23:04:57 +0800

    滑动窗口最大值

--- src/main/java/com/qiyu/leetcode/LeetCodeApplication.java
+++ src/main/java/com/qiyu/leetcode/LeetCodeApplication.java
@@ -4,5 +4,7 @@ public class LeetCodeApplication {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        solution.maxSlidingWindow(nums, 3);
     }
 }
--- src/main/java/com/qiyu/leetcode/Solution.java
+++ src/main/java/com/qiyu/leetcode/Solution.java
@@ -2,12 +2,46 @@ package com.qiyu.leetcode;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class Solution {
 
+
+    /**
+     * 优先级队列, 大顶堆
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1]);
+        for (int i = 0; i < k; i++) {
+            pq.add(new int[] {nums[i], i});
+        }
+
+        int[] ans = new int[nums.length - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < nums.length; i++) {
+            pq.add(new int[] {nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
+    }
+
+    /**
+     * 单调队列
+     * @param nums
+     * @param k
+     * @return
+     */
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> dq = new LinkedList<>();
 
+        //初始化第一个滑动窗口
         for(int i = 0; i < k; i++) {
             while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
                 dq.pollLast();
@@ -16,13 +50,16 @@ public class Solution {
         }
 
         int[] ans = new int[nums.length - k + 1];
+        //第一个滑动窗口的值
         ans[0] = nums[dq.peekFirst()];
         for(int i = k; i < nums.length; i++) {
+            //移除所有比当前小的值, 保持单调性
             while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
                 dq.pollLast();
             }
             dq.offerLast(i);
 
+            //如果值超过k个, 则需要进行移除
             if(dq.peekFirst() <= i - k) {
                 dq.pollFirst();
             }
