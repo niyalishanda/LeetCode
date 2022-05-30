87a49334acd42c315c7461958e5c91d0e817514d
niyalishanda <35219349+niyalishanda@users.noreply.github.com>
2022-05-31 00:32:04 +0800

    初始化

--- src/main/java/com/qiyu/leetcode/Solution.java
+++ src/main/java/com/qiyu/leetcode/Solution.java
@@ -1,40 +1,7 @@
 package com.qiyu.leetcode;
 
 class Solution {
-    int ans = Integer.MIN_VALUE;
     public void exec() {
         
     }
-
-    public int maxPathSum(TreeNode root) {
-        dfs(root);
-        return ans;
-    }
-
-    private int dfs(TreeNode node) {
-        if(node == null) {
-            return 0;
-        }
-
-        int left = Math.max(0, dfs(node.left));
-        int right = Math.max(0, dfs(node.right));
-
-        ans = Math.max(ans, node.val + left + right);
-
-
-        return Math.max(left, right) + node.val;
-    }
-
-     public class TreeNode {
-         int val;
-         TreeNode left;
-         TreeNode right;
-         TreeNode() {}
-         TreeNode(int val) { this.val = val; }
-         TreeNode(int val, TreeNode left, TreeNode right) {
-             this.val = val;
-             this.left = left;
-             this.right = right;
-         }
-     }
 }
