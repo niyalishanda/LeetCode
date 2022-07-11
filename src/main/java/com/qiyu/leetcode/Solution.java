e3ec73693942e283e1315f0c70a5943e657f44b8
niyalishanda <35219349+niyalishanda@users.noreply.github.com>
2022-07-12 00:15:16 +0800

    二叉树的锯齿形层序遍历

--- src/main/java/com/qiyu/leetcode/Solution.java
+++ src/main/java/com/qiyu/leetcode/Solution.java
@@ -1,7 +1,47 @@
 package com.qiyu.leetcode;
 
+import java.util.*;
+
 class Solution {
     public void exec() {
-        
+
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        boolean isOrderLeft = true;
+
+        while (!queue.isEmpty()) {
+            Deque<Integer> levelResult = new LinkedList<>();
+
+            for (int i = 0; i < queue.size(); i++) {
+                TreeNode currNode = queue.poll();
+                if (isOrderLeft) {
+                    levelResult.offerLast(currNode.val);
+                } else {
+                    levelResult.offerFirst(currNode.val);
+                }
+
+                if (currNode.left != null) {
+                    queue.offer(currNode.left);
+                }
+
+                if (currNode.right != null) {
+                    queue.offer(currNode.right);
+                }
+            }
+
+            result.add(new LinkedList<>(levelResult));
+            isOrderLeft = !isOrderLeft;
+        }
+
+        return result;
     }
 }
